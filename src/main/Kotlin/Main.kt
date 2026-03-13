9f28689b7f0ed95e0efe4107852751e5b1d77f8e
Wendy Cardenas Villalobos <wendycardenascpns@gmail.com>
2026-03-13 12:18:26 -0500

    List of the roles and players

--- src/main/Kotlin/Main.kt
+++ src/main/Kotlin/Main.kt
@@ -1,8 +1,6 @@
 import controller.MatchController
 import models.Match
 
-
-
 fun main() {
     var numberPlayers = 0
     println("-------Welcome to the soccer game!---------")
@@ -15,12 +13,9 @@ fun main() {
             println("Error: Invalid number. Let's try again!!!.\n")
         }
     }
-
     val matchController = MatchController()
     println("Please write the name for the first team")
     var name = readln()
-
-
     val teamA = matchController.createTeam(name, numberPlayers)
 
     println("----------------------------------------")
@@ -28,12 +23,7 @@ fun main() {
     name = readln()
     println("Now the players for the team $name:")
 
-    // Usamos la misma instancia para el segundo equipo
     val teamB = matchController.createTeam(name, numberPlayers)
-
-    // 3. SEGUNDO PASO CLAVE: Creamos el modelo Match con los equipos, NO el controlador
     val match = Match(teamA, teamB)
-
-    // 4. Le pedimos al controlador que juegue el partido que acabamos de crear
     matchController.playMatch(match)
 }
