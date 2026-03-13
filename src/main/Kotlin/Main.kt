32c985b5243e079e4000a34ba62492ceac587bcf
Wendy Cardenas Villalobos <wendycardenascpns@gmail.com>
2026-03-13 10:17:09 -0500

    The logic of the main and controller for the teams and players is organized, and the outputs and logic are improved

--- src/main/Kotlin/Main.kt
+++ src/main/Kotlin/Main.kt
@@ -1,63 +1,28 @@
-package main
+import controller.Match
+import controller.createTeam
 
-import game.Match
-import models.Team
-import models.Defender
-import models.Midfielder
-import models.Forward
-import models.Player
+
 
 fun main() {
-    lateinit var teamA :Team
-    lateinit  var teamB : Team
-
-    fun createPlayer(number: Int): Player {
-        println("Name of the player $number :")
-        val name = readln()
-
-        println("Select a type of the player:")
-        println("1 - Defender")
-        println("2 - Midfielder")
-        println("3 - Forward")
-
-        val type = readln().toInt()
-
-        return when (type) {
-            1 -> Defender(name)
-            2 -> Midfielder(name)
-            else -> Forward(name)
-        }
-    }
+    var numberPlayers = 0
     println("-------Welcome to the soccer game!---------")
-    println("Please select a number between 5-10 this going to be the number of players for the Team A:")
-    val numberPlayersA =readln().toInt()
-
-    if (numberPlayersA !in 5..10) {
-        println("Number of players needs to be between 5 and 10")
-        return
-    }else {
-        val playersA = mutableListOf<Player>()
-        for (i in 1..numberPlayersA) {
-            playersA.add(createPlayer(i))
+    while (true) {
+        println("Please select a number between 5-10. This is going to be the number of players for the teams:")
+        numberPlayers = readln().toInt()
+        if (numberPlayers in 5..10) {
+            break
+        } else {
+            println("Error: Invalid number. Let's try again!!!.\n")
         }
-        teamA = Team("TeamA", playersA)
     }
+    println("Please write the name for the first team")
+    var name = readln()
+    val teamA=createTeam(name,numberPlayers)
     println("----------------------------------------")
-    println("Now select a number between 5-10 this going to the number of players for the Team B:")
-
-    val numberPlayersB =readln().toInt()
-    if (numberPlayersB !in 5..10)  {
-        println("Number of players needs to be between 5 and 10")
-       return
-    }else {
-        val playersB = mutableListOf<Player>()
-        for (i in 1..numberPlayersB) {
-            playersB.add(createPlayer(i))
-        }
-
-        teamB = Team("TeamB", playersB)
-    }
-
+    println("Please write the name for the second team")
+    name = readln()
+    println("Now the players for the team $name:")
+    val teamB = createTeam(name,numberPlayers)
     val match = Match(teamA, teamB)
     match.playMatch()
 }
--- src/main/Kotlin/controller/Match.kt
+++ src/main/Kotlin/controller/Match.kt
@@ -1,13 +1,19 @@
-package game
+package controller
 
+import models.Defender
+import models.Forward
+import models.Midfielder
 import models.Player
 import models.Team
+
+
 
 class Match (val team1: Team,val team2: Team) {
  var score1 = 0
  var score2 = 0
-        fun selectRol(player1: Player, player2: Player, i: Int) {
-        println("los equipos son attack $player1 Vs Defender $player2")
+
+    fun selectRol(player1: Player, player2: Player, name1: String, name2: String, i: Int) {
+        println("The teams are attack $name1 Vs Defender $name2")
         println("Round# ${i + 1} the players attack: ${player1.name} Vs Defender ${player2.name}")
         if (player1.attack > player2.defense) {
             score1 += 1
@@ -26,13 +32,42 @@ class Match (val team1: Team,val team2: Team) {
             if(i%2 == 0) {
                 val attacker = team1.players[i]
                 val defender = team2.players[i]
-                selectRol(attacker, defender, i)
+                selectRol(attacker, defender,team1.name,team2.name, i)
             }else{
                 val attacker = team2.players[i]
                 val defender = team1.players[i]
-               selectRol(attacker, defender, i)
+               selectRol(attacker, defender,team2.name,team1.name, i)
             }
         }
         println("the final score is ${team1.name}: $score1 - ${team2.name}: $score2")
     }
 }
+fun createPlayer(number: Int): Player {
+    println("Name of the player $number :")
+    val name = readln()
+
+    println("Select a type of the player:")
+    println("1 - Defender")
+    println("2 - Midfielder")
+    println("3 - Forward")
+
+    val type = readln().toInt()
+
+    return when (type) {
+        1 -> Defender(name)
+        2 -> Midfielder(name)
+        else -> Forward(name)
+    }
+}
+
+fun createTeam(name: String, number: Int): Team {
+    val players = mutableListOf<Player>()
+
+    for (i in 1..number) {
+        players.add(createPlayer(i))
+    }
+
+    return Team(name, players)
+}
+
+
